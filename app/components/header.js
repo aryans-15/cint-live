957da92a6d8f5df076581b87ac74d03d088a2dc4
KyraCoding <pixelesc.com@gmail.com>
2025-02-26 12:51:59 -0500

    Enhance header component: add click outside handler for logout confirmation and improve user greeting

--- app/components/header.js
+++ app/components/header.js
@@ -3,7 +3,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faChartSimple, faCode, faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useUserSession } from '@/hooks/use-user-session';
 import { signInWithGoogle, signOutWithGoogle } from '@/libs/firebase/auth';
 import { createSession, removeSession } from '@/actions/auth-actions';
@@ -11,10 +11,10 @@ import { useAuthUser } from '@/hooks/use-auth-user';
 
 
 export default function Header({ session }) {
+    const elementRef = useRef(null);
     const useruid = useUserSession(session);
     const user = useAuthUser(useruid);
     const [confirmLogout, setConfirmLogout] = useState(false);
-    console.log(user);
     const handleSignIn = async () => {
         const uid = await signInWithGoogle();
         if (uid) {
@@ -26,6 +26,20 @@ export default function Header({ session }) {
         await signOutWithGoogle();
         await removeSession();
     };
+
+    useEffect(() => {
+        const handleClick = (event) => {
+            if (confirmLogout && (!elementRef.current || !elementRef.current.contains(event.target))) {
+                setConfirmLogout(false);
+            }
+        };
+
+        document.addEventListener("click", handleClick);
+        return () => {
+            document.removeEventListener("click", handleClick);
+        };
+    }, [confirmLogout]);
+
     return (
         <div className="flex w-full h-20 gap-4 z-10">
             <div className="flex justify-start grow">
@@ -56,11 +70,11 @@ export default function Header({ session }) {
                         <div className="flex-row h-full">
                             <div className={`py-2 px-4 items-center flex  h-full rounded-xl cursor-pointer ${confirmLogout ? "bg-gray-700" : ""} hover:bg-gray-700  transition duration-300 ease-in-out`}>
                                 <img src={user.photoURL} alt="User Avatar" className="aspect-square h-full rounded-full mr-2" />
-                                <p className="pl-2 font-bold">Hi, {user.displayName}</p>
+                                <p className="pl-2 font-bold">Hi {user.displayName.split(" ")[0]}!</p>
                             </div>
                         </div>
                         {confirmLogout && (
-                            <div onClick={handleSignOut} className={`${confirmLogout? "animate-fadeIn" : ""} font-bold cursor-pointer flex w-full h-fit p-2 rounded-xl bg-rose-600 hover:bg-rose-700 transition duration-300 ease-in-out mt-1`}>
+                            <div ref={elementRef} onClick={handleSignOut} className={`${confirmLogout ? "animate-fadeIn" : ""} font-bold cursor-pointer flex w-full h-fit p-2 rounded-xl bg-rose-600 hover:bg-rose-700 transition duration-300 ease-in-out mt-1`}>
                                 <p className="text-center w-full">Logout</p>
                             </div>
                         )}
